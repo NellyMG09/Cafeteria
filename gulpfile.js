2969632692244f32eed0babc11e8fd6e8f629c08
NellyMG09 <ingnellymendezg@gmail.com>
2025-07-30 16:39:43 -0600

    SASS con GULP

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const { src, dest, watch } = require('gulp'); //Con llaves exporta varias funciones
+const sass  = require('gulp-sass')(require('sass')); //Sin llave exporta una función 
+
+function css( done ) {
+    //compilar sass
+    //Paso 1 - Identificar hoja de archivo, 2 - Compilarla, 3 - Guardar el .css
+
+    src('src/scss/app.scss')
+    .pipe( sass() )
+    .pipe( dest('build/css') )
+
+    done();
+
+}
+
+function dev() {
+    watch('src/scss/app.scss', css);
+}
+
+exports.css = css;
+exports.dev = dev;
