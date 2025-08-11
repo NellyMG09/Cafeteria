65294f3ca7c90e19ddd6015bb6347579e3407fe5
NellyMG09 <ingnellymendezg@gmail.com>
2025-08-11 16:56:55 -0600

    Autoprefixr

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const { src, dest, watch } = require('gulp'); //Con llaves exporta varias funciones
 const sass  = require('gulp-sass')(require('sass')); //Sin llave exporta una función 
+const postcss = require('gulp-postcss');
+const autoprefixer = require ('autoprefixer');
+
 
 function css( done ) {
     //compilar sass
     //Paso 1 - Identificar hoja de archivo, 2 - Compilarla, 3 - Guardar el .css
 
     src('src/scss/app.scss')
-    .pipe( sass({ outputStyle: 'compressed'}) )
+    .pipe( sass() )
+    .pipe(postcss([ autoprefixer() ]))
     .pipe( dest('build/css') )
 
     done();
