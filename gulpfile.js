e6956812de4db95757f848497a8ae8cb52620601
NellyMG09 <ingnellymendezg@gmail.com>
2025-08-22 17:45:55 -0600

    Imagenes con Gulp2

--- gulpfile.js
+++ gulpfile.js
@@ -5,43 +5,50 @@ const autoprefixer = require ('autoprefixer');
 
 //Imagenes
 const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
 
-function css( done ) {
+let webp = require('gulp-webp')
+webp = webp.default || webp
+
+const avif = require('gulp-avif');
+
+function css() {
     //compilar sass
     //Paso 1 - Identificar hoja de archivo, 2 - Compilarla, 3 - Guardar el .css
-
-    src('src/scss/app.scss')
-    .pipe( sass() )
-    .pipe(postcss([ autoprefixer() ]))
-    .pipe( dest('build/css') )
-
-    done();
-
+    return src('src/scss/app.scss')
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest('build/css'))
 }
 
-function imagenes ( ) {
-    src('src/img/**/*')
+function imagenes() {
+    return src('src/img/**/*')
     .pipe( imagemin({ optimizationLevel: 3 }) )
     .pipe( dest('build/img') );
 }
 
 function versionWebp() {
     return src('src/img/**/*.{png,jpg}')
-     .pipe( webp() )
-     .pipe( dest('build/img') )
+     .pipe(webp())
+     .pipe(dest('build/img'))
+}
+
+function versionAvif() {
+    return src('src/img/**/*.{png,jpg}')
+       .pipe(webp({avif: true})) //converte a AVIF con gulp-webp
+       .pipe(dest('build/img'))
 }
 
 function dev() {
     watch('src/scss/**/*.scss', css);
-    watch('src/img/**/*',imagenes);
+    watch('src/img/**/*',series(imagenes, versionWebp, versionAvif));
 }
 
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.default = series( imagenes, versionWebp, css, dev );
+exports.versionAvif = versionAvif;
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
 
 // series - Se inicia una tarea y hasta que finaliza, inicia la siguiente
 
